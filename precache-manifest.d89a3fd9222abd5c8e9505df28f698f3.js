2ba76da1c5c5ad98b95e5c8f444533035760de22
unknown <mirukusheki@gmail.com>
2019-01-25 16:42:27 +0700

    Updates

---
+++ precache-manifest.d89a3fd9222abd5c8e9505df28f698f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "9c5273d1463d01f49c7c",
+    "url": "/contactmanager/static/js/main.9c5273d1.chunk.js"
+  },
+  {
+    "revision": "30d9b22e2b86faf95634",
+    "url": "/contactmanager/static/js/1.30d9b22e.chunk.js"
+  },
+  {
+    "revision": "9c5273d1463d01f49c7c",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "30d9b22e2b86faf95634",
+    "url": "/contactmanager/static/css/1.9bc6279e.chunk.css"
+  },
+  {
+    "revision": "238a80cd0a00acd0a603506e3ca75a8f",
+    "url": "/contactmanager/index.html"
+  }
+];
